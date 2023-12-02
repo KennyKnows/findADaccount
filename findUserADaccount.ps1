b80cb10ebfe9fabc5939844dad75edf676f02afa
Kevin Ngo <66854579+KennyKnows@users.noreply.github.com>
2023-12-02 03:08:43 -0500

    Add files via upload

---
+++ findUserADaccount.ps1
@@ -0,0 +1,3 @@
+﻿Get-ADUser -Filter {(GivenName -like "FirstName") -and (Surname -like "LastName")} -Properties * | 
+
+fl name, DisplayName, enabled, lockedout, Office, Department, lastlogondate, mail, officePhone, title, info, passwordlastset
