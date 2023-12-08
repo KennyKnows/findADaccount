4d3b16fa19320332a108829c6ae463b244ffffdc
Kevin Ngo <66854579+KennyKnows@users.noreply.github.com>
2023-12-08 15:03:57 -0500

    Update findUserADaccount.ps1

--- findUserADaccount.ps1
+++ findUserADaccount.ps1
@@ -1,5 +1,5 @@
-#Run script in your domain controller.
-#Replace FirstName and LastName of desired user.
+#Run script in host with Active Directory.
+#Replace FirstName and LastName of the desired user.
  Get-ADUser -Filter {(GivenName -like "FirstName") -and (Surname -like "LastName")} -Properties * | 
 
 fl name, DisplayName, enabled, lockedout, Office, Department, lastlogondate, mail, officePhone, title, info, passwordlastset
